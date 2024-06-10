4e97afbb850a74771e8bf17cc2922fee8ad2690e
bascome <eva.bascompte.moragas@vumc.org>
2024-06-10 08:32:12 -0500

    php 8 errors and extra .pdf deleted

--- classes/ProjectData.php
+++ classes/ProjectData.php
@@ -45,72 +45,80 @@ class ProjectData
         foreach ($filterLogic as $filterkey => $filtervalue){
             array_push($found, false);
         }
-        foreach ($records as $record=>$record_array) {
-            $count = 0;
-            foreach ($filterLogic as $filterkey => $filtervalue){
-                $found[$count] = false;
-                $count++;
-            }
-            foreach ($record_array as $event=>$data) {
-                if($event == 'repeat_instances'){
-                    foreach ($data as $eventarray){
-                        $datarepeat = array();
-                        foreach ($eventarray as $instrument=>$instrumentdata){
-                            $count = 0;
-                            foreach ($instrumentdata as $instance=>$instancedata){
-                                foreach ($instancedata as $field_name=>$value){
-                                    if(!array_key_exists($field_name,$array[$index])){
-                                        $array[$index][$field_name] = array();
-                                    }
-
-                                    if($value != "" && (!is_array($value) || (is_array($value) && !empty($value)))){
-                                        $datarepeat[$field_name][$instance] = $value;
-                                        $count = 0;
-                                        foreach ($filterLogic as $filterkey => $filtervalue){
-                                            if($value == $filtervalue && $field_name == $filterkey){
-                                                $found[$count] = true;
-                                            }
-                                            $count++;
+        if(is_array($records) && !empty($records)) {
+            foreach ($records as $record => $record_array) {
+                $count = 0;
+                if (is_array($filterLogic) && !empty($filterLogic)) {
+                    foreach ($filterLogic as $filterkey => $filtervalue) {
+                        $found[$count] = false;
+                        $count++;
+                    }
+                }
+                foreach ($record_array as $event => $data) {
+                    if ($event == 'repeat_instances') {
+                        foreach ($data as $eventarray) {
+                            $datarepeat = array();
+                            foreach ($eventarray as $instrument => $instrumentdata) {
+                                $count = 0;
+                                foreach ($instrumentdata as $instance => $instancedata) {
+                                    foreach ($instancedata as $field_name => $value) {
+                                        if (!array_key_exists($field_name, $array[$index])) {
+                                            $array[$index][$field_name] = array();
                                         }
+
+                                        if ($value != "" && (!is_array($value) || (is_array($value) && !empty($value)))) {
+                                            $datarepeat[$field_name][$instance] = $value;
+                                            $count = 0;
+                                            if (is_array($filterLogic) && !empty($filterLogic)) {
+                                                foreach ($filterLogic as $filterkey => $filtervalue) {
+                                                    if ($value == $filtervalue && $field_name == $filterkey) {
+                                                        $found[$count] = true;
+                                                    }
+                                                    $count++;
+                                                }
+                                            }
+                                        }
+
                                     }
-
+                                    $count++;
                                 }
-                                $count++;
+                            }
+                            foreach ($datarepeat as $field => $datai) {
+                                #check if non repeatable value is empty and add repeatable value
+                                #empty value or checkboxes
+                                if ($array[$index][$field] == "" || (is_array($array[$index][$field]) && empty($array[$index][$field][1]))) {
+                                    $array[$index][$field] = $datarepeat[$field];
+                                }
                             }
                         }
-                        foreach ($datarepeat as $field=>$datai){
-                            #check if non repeatable value is empty and add repeatable value
-                            #empty value or checkboxes
-                            if($array[$index][$field] == "" || (is_array($array[$index][$field]) && empty($array[$index][$field][1]))){
-                                $array[$index][$field] = $datarepeat[$field];
+                    } else {
+                        $array[$index] = $data;
+                        foreach ($data as $fname => $fvalue) {
+                            $count = 0;
+                            if (is_array($filterLogic) && !empty($filterLogic)) {
+                                foreach ($filterLogic as $filterkey => $filtervalue) {
+                                    if ($fvalue == $filtervalue && $fname == $filterkey) {
+                                        $found[$count] = true;
+                                    }
+                                    $count++;
+                                }
                             }
                         }
                     }
-                }else{
-                    $array[$index] = $data;
-                    foreach ($data as $fname=>$fvalue) {
-                        $count = 0;
-                        foreach ($filterLogic as $filterkey => $filtervalue){
-                            if($fvalue == $filtervalue && $fname == $filterkey){
-                                $found[$count] = true;
-                            }
-                            $count++;
-                        }
+                }
+                $found_total = true;
+                foreach ($found as $fname => $fvalue) {
+                    if ($fvalue == false) {
+                        $found_total = false;
+                        break;
                     }
                 }
-            }
-            $found_total = true;
-            foreach ($found as $fname=>$fvalue) {
-                if($fvalue == false){
-                    $found_total = false;
-                    break;
+                if (!$found_total && $filterLogic != null) {
+                    unset($array[$index]);
                 }
-            }
-            if(!$found_total && $filterLogic != null){
-                unset($array[$index]);
-            }
 
-            $index++;
+                $index++;
+            }
         }
         return $array;
     }
