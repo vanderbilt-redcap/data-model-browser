43e8da708345e59f1e2464fdaeabb8959ab718fc
bascome <eva.bascompte.moragas@vumc.org>
2024-06-11 09:16:09 -0500

    bug fix on file

--- downloadFile.php
+++ downloadFile.php
@@ -2,12 +2,22 @@
 namespace Vanderbilt\DataModelBrowserExternalModule;
 require_once "projects.php";
 
+//$filename = db_escape($_REQUEST['file']);
+//$sname = db_escape($_REQUEST['sname']);
+//
+//header('Content-type: application/pdf');
+//header('Content-Disposition: attachment; filename="'.$filename.'"');
+//header('Content-Transfer-Encoding: binary');
+//header('Accept-Ranges: bytes');
+//@readfile($module->framework->getSafePath($sname, EDOC_PATH));
+
 $filename = db_escape($_REQUEST['file']);
 $sname = db_escape($_REQUEST['sname']);
+$extension = pathinfo($filename, PATHINFO_EXTENSION);
 
-header('Content-type: application/pdf');
+header('Content-type: application/'.$extension);
 header('Content-Disposition: attachment; filename="'.$filename.'"');
 header('Content-Transfer-Encoding: binary');
 header('Accept-Ranges: bytes');
-@readfile($module->framework->getSafePath($sname, EDOC_PATH));
+@readfile($module->framework->getSafePath(EDOC_PATH.$sname, EDOC_PATH));
 ?>
