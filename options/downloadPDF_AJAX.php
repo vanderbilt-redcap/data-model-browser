2bd236ce1479867fe8d1e7edb97f580cd72e79eb
bascome <eva.bascompte.moragas@vumc.org>
2025-07-14 15:24:15 -0500

    changed excel for csv

--- options/downloadPDF_AJAX.php
+++ options/downloadPDF_AJAX.php
@@ -6,55 +6,49 @@ require_once __DIR__ .'/../vendor/autoload.php';
 include_once __DIR__ ."/../functions.php";
 
 use PhpOffice\PhpSpreadsheet\Spreadsheet;
-use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
-
-$RecordSetSettings = \REDCap::getData($pidsArray['SETTINGS'], 'array');
-$settings = ProjectData::getProjectInfoArrayRepeatingInstruments($RecordSetSettings,$pidsArray['SETTINGS'])[0];
-
-$option = $_REQUEST['option'];
-$deprecated = $_REQUEST['deprecated'];
-$draft = $_REQUEST['draft'];
+use PhpOffice\PhpSpreadsheet\Writer\Csv;
 
 $RecordSetDataModel = \REDCap::getData($pidsArray['DATAMODEL'], 'array');
 $dataTable = ProjectData::getProjectInfoArrayRepeatingInstruments($RecordSetDataModel,$pidsArray['DATAMODEL']);
 $requested_tables = getHtmlTableCodesTableArrayExcel($module,$dataTable,$pidsArray);
 
 #EXEL SHEET
-$filename = "CodeList_ " . date("Y-m-d_hi",time()) . ".xlsx";
+$filename = "CodeList_ " . date("Y-m-d_hi",time()) . ".csv";
 
-$styleArray = array(
-    'font'  => array(
-        'size'  => 10,
-        'name'  => 'Calibri'
-    ),
-    'alignment' => array(
-        'vertical' => \PhpOffice\PhpSpreadsheet\Style\Alignment::VERTICAL_CENTER,
-    ));
-
+// Create a new spreadsheet instance
 $spreadsheet = new Spreadsheet();
-$spreadsheet->getDefaultStyle()->applyFromArray($styleArray);
 $sheet = $spreadsheet->getActiveSheet();
 
-///MULTIREG CONCEPTS///
 #SECTION HEADERS
-$section_headers = array(0=>"Table",1=>"Variable",2=>"Code",3=>"Label");
-$section_headers_leters = array(0=>'A',1=>'B',2=>'C',3=>'D');
-$section_headers_width = array(0=>'20',1=>'30',2=>'20',3=>'40');
-$section_centered = array(0=>'0',1=>'0',2=>'1',3=>'0');
+$sectionHeaders = [0=>"Table",1=>"Variable",2=>"Code",3=>"Label"];
+$sectionLeters = [0=>'A',1=>'B',2=>'C',3=>'D'];
+
 $row_number = 1;
-$sheet = getExcelHeaders($sheet,$section_headers,$section_headers_leters,$section_headers_width,$row_number);
-$sheet->setAutoFilter('A1:D1');
+foreach ($sectionHeaders as $key => $value) {
+    $sheet->setCellValue($sectionLeters[$key].$row_number, $value);
+}
+
+#DATA
 $row_number++;
-$sheet = getExcelData($sheet,$requested_tables,$section_headers,$section_headers_leters,$section_centered,$row_number);
+foreach ($requested_tables as $row => $data) {
+    foreach ($sectionHeaders as $index => $header) {
+        $sheet->setCellValue($sectionLeters[$index].$row_number, $data[$index]);
+    }
+    $row_number++;
+}
 
-#Rename sheet
-$sheet->setTitle('Codes');
+// Save the spreadsheet as a CSV file
+$writer = new Csv($spreadsheet);
 
-$writer = new Xlsx($spreadsheet);
+// Set CSV-specific options
+$writer->setDelimiter(','); // Default is ','
+$writer->setEnclosure('"'); // Default is '"'
+$writer->setLineEnding("\r\n"); // Default is PHP_EOL
+$writer->setSheetIndex(0); // Default is 0
 
+//Download file
 header('Content-Type: application/vnd.ms-excel');
 header('Content-Disposition: attachment; filename="'.$filename.'"');
 $writer->save("php://output");
-
-
 ?>
+
