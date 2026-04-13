b43eb3c425b6391f34bee1218e7ccaff9ee73a08
bascome <eva.bascompte.moragas@vumc.org>
2026-04-13 10:27:15 -0500

    excel improvements

--- functions.php
+++ functions.php
@@ -220,47 +220,6 @@ function getHtmlCodesTableArrayExcel($module, $data_array,$data_code_array,$code
     return $data_array;
 }
 
-/***PHP SPREADSHEET***/
-
-function getExcelHeaders($sheet,$headers,$letters,$width,$row_number){
-    foreach ($headers as $index=>$header) {
-        $sheet->setCellValue($letters[$index] . $row_number, $header);
-        $sheet->getStyle($letters[$index] . $row_number)->getFill()->setFillType(\PhpOffice\PhpSpreadsheet\Style\Fill::FILL_SOLID);
-        $sheet->getStyle($letters[$index].$row_number)->getFill()->getStartColor()->setARGB('4db8ff');
-        $sheet->getStyle($letters[$index].$row_number)->getFont()->setBold( true );
-        $sheet->getStyle($letters[$index].$row_number)->getBorders()->getAllBorders()->setBorderStyle(\PhpOffice\PhpSpreadsheet\Style\Border::BORDER_MEDIUM);
-        $sheet->getStyle($letters[$index].$row_number)->getAlignment()->setHorizontal('center');
-        $sheet->getStyle($letters[$index].$row_number)->getAlignment()->setWrapText(true);
-
-        $sheet->getColumnDimension($letters[$index])->setAutoSize(false);
-        $sheet->getColumnDimension($letters[$index])->setWidth($width[$index]);
-    }
-    return $sheet;
-}
-
-function getExcelData($sheet,$data_array,$headers,$letters,$section_centered,$row_number){
-    $active_n_found = false;
-    foreach ($data_array as $row => $data) {
-        foreach ($headers as $index => $header) {
-            $sheet->setCellValue($letters[$index].$row_number, $data[$index]);
-            $sheet->getStyle($letters[$index].$row_number)->getBorders()->getAllBorders()->setBorderStyle(\PhpOffice\PhpSpreadsheet\Style\Border::BORDER_THIN);
-            $sheet->getStyle($letters[$index].$row_number)->getAlignment()->setWrapText(true);
-            if($section_centered[$index] == "1"){
-                $sheet->getStyle($letters[$index].$row_number)->getAlignment()->setHorizontal('center');
-            }
-        }
-        if( $active_n_found){
-            foreach ($headers as $index=>$header) {
-                $sheet->getStyle($letters[$index] . $row_number)->getFill()->setFillType(\PhpOffice\PhpSpreadsheet\Style\Fill::FILL_SOLID);
-                $sheet->getStyle($letters[$index].$row_number)->getFill()->getStartColor()->setARGB('e6e6e6');
-            }
-            $active_n_found = false;
-        }
-        $row_number++;
-    }
-    return $sheet;
-}
-
 /**
  * Table list with anchor links for the JsonPDF
  * @param $dataTable
